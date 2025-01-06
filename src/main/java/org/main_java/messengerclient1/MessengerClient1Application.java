ae582105b2d8b7737618e3bc6bdd09296c7b6b78
Jaime López <jlopedia@myuax.com>
2025-01-06 16:37:32 +0100

    Initial commit

---
+++ src/main/java/org/main_java/messengerclient1/MessengerClient1Application.java
@@ -0,0 +1,13 @@
+package org.main_java.messengerclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerClient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerClient1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/main_java/messengerclient1/MessengerClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.main_java.messengerclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerClient1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
