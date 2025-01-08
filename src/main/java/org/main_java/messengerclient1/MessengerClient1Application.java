7e40b17b7e675bec7f5fb193f8b34443d933c7f8
marcos <marcgabe@myuax.com>
2025-01-08 18:53:47 +0100

    Update MessengerClient1Application.java
    Para el descubrimiento del cliente por el server

--- src/main/java/org/main_java/messengerclient1/MessengerClient1Application.java
+++ src/main/java/org/main_java/messengerclient1/MessengerClient1Application.java
@@ -2,7 +2,9 @@ package org.main_java.messengerclient1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MessengerClient1Application {
 
